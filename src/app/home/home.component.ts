73a061d47e9c4a9e1d3ab4b67da137f0ed9d1c71
Richard Ueckermann <rueckermann@sse.co.za>
2018-02-28 15:54:19 +0200

    add classes to home component for navbar and footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy, Renderer2 } from "@angular/core";
 
 @Component({
   selector: "home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  // has-navbar-fixed-top has-footer-bar
-
-  constructor() { }
-
+  constructor(private renderer: Renderer2) {
+    this.renderer.addClass(document.body, "has-navbar-fixed-top");
+    this.renderer.addClass(document.body, "has-footer-bar");
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.renderer.removeClass(document.body, "has-navbar-fixed-top");
+    this.renderer.removeClass(document.body, "has-footer-bar");
   }
 
 }
