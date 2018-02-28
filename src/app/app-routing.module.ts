69e6af731a70bf9abb6361d3f8911715eaf7f4f0
Richard Ueckermann <rueckermann@sse.co.za>
2018-02-28 13:13:59 +0200

    get login and routing working with auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
+import { AuthGuard } from "./core/auth.guard";
+
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+
 const routes: Routes = [
-  
+  { path: "", component: HomeComponent, canActivate: [ AuthGuard ] },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [ AuthGuard ]
 })
 export class AppRoutingModule { }
--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/do";
@@ -11,7 +11,7 @@ import { AuthService } from "./auth.service";
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
     return this.auth.user$
@@ -19,7 +19,7 @@ export class AuthGuard implements CanActivate {
       .map(user => !!user)
       .do(loggedIn => {
         if (!loggedIn) {
-          console.log("access denied");
+          this.router.navigate(["/login"]);
         }
       });
   }
